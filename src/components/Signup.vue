e09cfc9a7573d49eea212460a87c493052a8ddd3
lc1024 <b02611002@ntu.edu.tw>
2017-01-24 16:20:31 +0800

    Add components

---
+++ src/components/Signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>報名</h3>
+    <v-btn success large>立刻報名</v-btn>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Q1:'
+}
+</script>
+<style scoped>
+</style>
