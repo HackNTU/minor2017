c94bc69b56759b6f2996a3aa1a769f10a756244f
lc1024 <b02611002@ntu.edu.tw>
2017-02-09 19:08:56 +0800

    Fill Partners component with Airtable data. Lack RWD.

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,33 +1,57 @@
 <template>
   <div>
     <h3>合作夥伴</h3>
-    <pre>合作夥伴的一一介紹</pre>
-    <div class="">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-    <v-container fluid="fluid">
-      <v-row>
+    <h4>副標題</h4>
 
-        <v-col xs2="xs2" v-for="i in numbers">
-          <v-card class="primary">
-            <v-card-text>2</v-card-text>
-            {{i}}
+    <!-- TODO: simple version template, delete later if not needed -->
+    <!-- <v-container>
+      <div v-for="partner in partners">
+        <p>Name: {{ partner.get('Name') }}</p>
+        <img :src="partner.get('Photo')[0].url" height="75px">
+        <p>Description: {{ partner.get('Description') }}</p>
+      </div>
+    </v-container> -->
+
+    <!-- TODO: card version template, delete later if not needed -->
+    <v-container>
+      <div v-for="partner in partners">
+        <div class="partner">
+          <v-card horizontal>
+            <v-card-row :img="partner.get('Photo')[0].url" height="120px"></v-card-row>
+            <v-card-column>
+              <v-card-row height="120px" class="brown white--text text-xs-left">
+                <v-card-text>
+                  <strong>{{ partner.get('Name') }}</strong>
+                  <v-spacer></v-spacer>
+                  <div>{{ partner.get('Description') }}</div>
+                </v-card-text>
+              </v-card-row>
+              <v-card-row actions class="brown darken-2">
+                <v-btn flat class="white--text">
+                  <!-- TODO: Find more icons in https://material.io/icons/#ic_language -->
+                  <v-icon left>language</v-icon>{{ partner.get('Link') }}
+                </v-btn>
+              </v-card-row>
+            </v-card-column>
           </v-card>
-        </v-col>
-
-      </v-row>
+        </div>
+      </div>
     </v-container>
+
   </div>
 </template>
+
 <script>
+import { partners } from '../service/airtable.js'
 export default {
   name: 'partners',
   data: function(){
     return {
-      numbers: [ 2, 3, 4, 5 ]
+      partners: partners
     }
   }
 }
 </script>
+
 <style scoped>
 </style>
