e8156c0885f7b5bcc6d40b0410425cb0ca0b7166
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2017-02-18 17:18:37 +0800

    [hotfix] relative path in App.vue (#5)
    Update News.vue component with link
    fix relative path in App.vue

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,35 +2,49 @@
   <div class="block">
     <!-- <h5 class="mt-5">最新消息</h5> -->
     <v-tabs id="mobile- tabs-6" scroll-bars class="mt-0">
-      <v-tab-item v-for="i in 13" v-bind:href="'#mobile-tabs-6-' + i" slot="activators">
-        <pre>  這是一條最新消息的標題！           </pre>
-      </v-tab-item>
-      <v-tab-content v-for="i in 13" v-bind:id="'mobile-tabs-6-' + i" slot="content">
-        <v-card>
-          <v-card-text><p>
-            這是一條最新消息的內容的內容，這是一條最新消息的內容的內容，這是一條最新消息的內容的內容，這是一條最新消息的內容的內容，這是一條最新消息的內容的內容，這是一條最新消息的內容的內容，這是一條最新消息的內容的內容，這是一條最新消息的內容的內容。詳情請見 https://facebook.com/hackntu
-          </p></v-card-text>
-        </v-card>
-      </v-tab-content>
-    </v-tabs>
+      <v-tab-item
+        v-for="a_piece_of_news in news"
+        v-bind:href="'#mobile-tabs-6-' + a_piece_of_news.index"
+        slot="activators">
 
+        <a :href="a_piece_of_news.get('Link')">
+          [{{ a_piece_of_news.get('Release') }}] {{ a_piece_of_news.get('Title') }}
+        </a>
+
+      </v-tab-item>
+    </v-tabs>
   </div>
 </template>
 
 <script>
+import { news } from '../service/airtable.js'
 export default {
   name: 'news',
   data: function(){
     return{
-
+      news: news,
+      l: news.length
     }
-  }
+  },
+  computed: {
+    indexedNews: function(){
+      var indexedNews = []
+      this.news.forEach(function (value, i) {
+        // console.log('%d: %s', i, value);
+        // console.log(this.news[i]);
+        indexedNews[i] = news[i]
+        indexedNews[i].index = i
+      });
+      return indexedNews
+    }
+  },
 }
 </script>
 
 <style scoped>
-p,v-card-text {
-  margin: auto;
-  max-width: 1000px;
+/* prevent default <a/> style from turning link invisible on tab */
+a {
+  color: white;
+  text-decoration: none;
 }
 </style>
